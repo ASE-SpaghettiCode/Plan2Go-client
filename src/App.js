9abd19fc05bc7d6405ac64c29dc02a1da9a7fa59
tluo3032 <tiantian.luo@uzh.ch>
2023-03-13 15:58:58 +0100

    initial front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Profile from "./components/Profile";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Profile</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/profile" element={<Profile/>}/>
+                <Route exact path="/landing" element={<Landing/>}/>
+            </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
