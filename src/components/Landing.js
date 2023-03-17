45a72bfff3a7bde604c4298a92fd948093868312
Duanran Jing <duanran.jing@outlook.com>
2023-03-17 21:40:19 +0100

    Implement onClick with buttons and logo

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import '../styles/Landing.css';
-import logo from '../images/Logo.png';
 import LandingLayout from "./LandingLayout";
+import { useNavigate } from 'react-router-dom';
+import {func} from "prop-types";
+
 
 const Landing = () => {
+    const navigate = useNavigate();
+    function handleLoginClick() {
+        navigate('/login');
+    }
+    function handleRegisterClick() {
+        navigate('/register');
+    }
     return (
         <LandingLayout>
             <p className={"title"}>Tell us about your last adventure</p>
             <p className={"title"}>and start your next today</p>
             <div className={"buttonContainer"}>
-                <button className={"loginButtonLanding"}>Log In</button>
-                <button className={"registerButtonLanding"}>Register</button>
+                <button className={"loginButtonLanding"} onClick={handleLoginClick}>Log In</button>
+                <button className={"registerButtonLanding"} onClick={handleRegisterClick}>Register</button>
             </div>
         </LandingLayout>
     );
