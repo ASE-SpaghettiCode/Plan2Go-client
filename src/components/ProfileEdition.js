9615c8a8f61b0ea32024e02beff92fa38a16361f
wenzli0510 <wenzhe.li@uzh.ch>
2023-03-26 19:35:28 +0200

    complete put method

--- src/components/ProfileEdition.js
+++ src/components/ProfileEdition.js
@@ -22,6 +22,8 @@ const ProfileEdition: React.FC = () => {
     const [image, setImage] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    const userId = localStorage.getItem("id");
+
     useEffect(() => {
         // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
         async function fetchData() {
@@ -83,6 +85,21 @@ const ProfileEdition: React.FC = () => {
         window.location.href = `/home`;
     }
 
+    const doSubmit = async () => {
+        try {
+            const requestBody = JSON.stringify({userId, username, password, intro});
+            console.log(requestBody);
+            await api.put(`/users/${userId}`, requestBody);
+
+            // submit successfully worked --> navigate to his/her own profile
+            let path = `/users/${userId}`;
+            window.location.href = path;
+
+        } catch (error) {
+            alert(`Something went wrong during the edit: \n${handleError(error)}`);
+        }
+    };
+
     return (
         <div>
             <Header style={{
@@ -95,7 +112,8 @@ const ProfileEdition: React.FC = () => {
                 <img src={logo} className={"naviLogo"} onClick={goHome}/>
                 <NaviBar style={{marginLeft: 'auto'}}/>
             </Header>
-            <form className={"editProfileForm"} style={{width: "400px", margin: "0 auto", height: "100vh"}}>
+            <form className={"editProfileForm"} onSubmit={doSubmit}
+                  style={{width: "400px", margin: "0 auto", height: "100vh"}}>
                 <div className={"formColor"}/>
                 <h2 className="titleEdit">Person info</h2>
                 <div>
@@ -110,14 +128,11 @@ const ProfileEdition: React.FC = () => {
                 <label className="label" htmlFor={"intro"}>Introduction</label>
                 <TextArea rows={3} className="inputIntro" type={"intro"} id={"intro"} value={intro}
                           onChange={(e) => setIntro(e.target.value)}/>
-                {/*<input className="inputIntro" type={"intro"} id={"intro"} value={intro}*/}
-                {/*       onChange={(e) => setIntro(e.target.value)}/>*/}
                 <div className={"buttonContainer"}>
-                    <button className={"loginButtonLogin"} type={"submit"}>Summit</button>
+                    <button className={"summitButton"} type={"submit"}>Summit</button>
                 </div>
                 <p className={"cancel"} style={{textAlign: "center", cursor: "pointer"}}><Link
                     to={`/users/${myUserId}`}>cancel</Link></p>
-                {/*<p className={"cancel"} style={{ textAlign: "center", cursor: "pointer" }}> <Link to={`/register`}>cancel</Link></p>*/}
             </form>
         </div>
     )
