cb80888ccb07bdcd6554232ff0ab631b51210747
Duanran Jing <duanran.jing@outlook.com>
2023-04-11 15:19:22 +0200

    Implement post delete

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,9 @@
 import '../styles/PostList.css'
-import {api_posts, handleError} from "../helpers/api";
+import {api, api_posts, handleError} from "../helpers/api";
 import {useState,useEffect} from "react";
 import React from "react";
+import User from "../models/user";
+import user from "./User";
 
 export default function PostList(){
     const path = window.location.pathname;
@@ -24,13 +26,27 @@ export default function PostList(){
     },[]);
 
 
+    const handleClick = async(props) => {
+        const userId = localStorage.getItem('userId', user.userId);
+        const postId = props.postId;
+        try {
+            const requestBody = JSON.stringify({userId,postId});
+            const response = await api.post('/users/{userId}/posts/{postId}', requestBody);
+            window.location.href = `/users/:id`;
+
+        } catch (error) {
+            alert(`Something went wrong during deleting the post: \n${handleError(error)}`);
+        }
+    }
+
+
     const postItems = Posts.map((post)=>
         <div className="postContainer">
             <div className="text">
                 {post.content}
             </div>
             <div className="delete">
-                <span className="post-delete">delete</span>
+                <span className="post-delete" onClick={() => handleClick(props)}>delete</span>
             </div>
         </div>
     );
