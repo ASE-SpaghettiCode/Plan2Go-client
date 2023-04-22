378ab3b93b56c2b9ca04dcec2b58269f00b94dcd
wenzli0510 <wenzhe.li@uzh.ch>
2023-04-22 12:52:35 +0200

    complete search box layout

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -2,15 +2,26 @@ import logo from "../images/Logo.png";
 import NaviBar from "./NaviBar";
 import React from "react";
 import {Header} from "antd/es/layout/layout";
+import SearchBox from "./SearchBox";
+import {Input} from "antd";
 
-const HeaderBar = () =>{
+const { Search } = Input;
+
+const HeaderBar = () => {
     const goHome = () => {
         window.location.href = `/home`;
     }
 
-    return (<Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', backgroundColor: 'white', width: '100%' }}>
+    return (<Header style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        backgroundColor: 'white',
+        width: '100%'
+    }}>
         <img src={logo} className={"naviLogo"} onClick={goHome}/>
-        <NaviBar style={{ marginLeft: 'auto' }} />
+        <SearchBox/>
+        <NaviBar style={{marginLeft: 'auto'}}/>
     </Header>)
 }
 
